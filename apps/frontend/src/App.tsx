ff962e21567129b04dec835345ab610a3922a53d
Bryand237 <masterminddelta237@gmail.com>
2025-10-17 14:16:58 +0100

    Initialisation

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="w-screen h-screen bg-blue-200 font-medium">
+      <p className="text-blue-700">Bonjour</p>
+    </div>
+  );
+};
+
+export default App;
